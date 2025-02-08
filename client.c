8b1a1844e9f91d7975645a7ce47afb6986a923b6
Szymon Madeja <szmadeja@c2r6s3.42warsaw.pl>
2025-02-08 18:38:35 +0100

    sdf

--- client.c
+++ client.c
@@ -6,8 +6,16 @@
 /*   By: szmadeja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:30:22 by szmadeja          #+#    #+#             */
-/*   Updated: 2025/02/07 17:30:36 by szmadeja         ###   ########.fr       */
+/*   Updated: 2025/02/08 17:35:01 by szmadeja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 2)
+		return (0);
+
+	return (0);
+}
